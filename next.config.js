9e79935cc6c81f25c34013fa8c3f2c63987217ab
Magalhaes <lucas.magalhaes@hp.com>
2022-08-29 14:30:07 -0300

    adding docker hot reload

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  webpackDevMiddleware: (config) => {
+    // Solve compiling problem via vagrant
+    config.watchOptions = {
+      poll: 1000,   // Check for changes every second
+      aggregateTimeout: 300,   // delay before rebuilding
+    };
+    return config;
+  }
 };
 
 module.exports = nextConfig;
